0aa3de91d2fae449f25330c24f4938c9c77a6a94
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2019-12-17 14:03:09 +0100

    Fire useFetchTopStories hook in TopStories SFC

--- src/components/blocks/TopStories/index.js
+++ src/components/blocks/TopStories/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-const TopStories = () => {
-  return <div></div>;
+import { useFetchTopStories } from "../../../helpers/useFetch";
+import { fetchTopStories } from "../../../actions/fetch";
+import { TOKEN } from "../../../token";
+
+const URL = "https://newsapi.org/v2/top-headlines?country=us";
+
+const TopStories = props => {
+  useFetchTopStories(`${URL}&apiKey=${TOKEN}`, fetchTopStories);
+  const topStories = useSelector(state => state.topStories);
+
+  return <div>Top Stories</div>;
 };
 
 export default TopStories;
